331362d6ceb8e9a1008c0239a7c44bb00a324e72
lynnfaraday <lomeara@gmail.com>
2012-07-24 02:32:52 -0400

    Adding pagination and moving widgets around.

--- plugins/mustache_args.rb
+++ plugins/mustache_args.rb
@@ -3,13 +3,10 @@ class Ruhoh
     module Helpers    
 
       GalleryRegex = /^build_gallery_(\d+)/
-      RecentPostsRegex = /^recent_posts_(\d+)/
 
       def respond_to?(method)
         if method.to_s =~ GalleryRegex          
           true
-        elsif method.to_s =~ RecentPostsRegex
-            true
         else
           super
         end
@@ -18,8 +15,6 @@ class Ruhoh
       def method_missing(name, *args, &block)
         if name.to_s =~ GalleryRegex
           self.build_gallery($1) || super
-        elsif name.to_s =~ RecentPostsRegex
-          self.recent_posts($1.to_i) || super
         else
           super
         end
--- plugins/obfuscate/obfuscate.rb
+++ plugins/obfuscate/obfuscate.rb
@@ -3,8 +3,10 @@ class Ruhoh
     module Pages
       def self.run(target, page)
         FileUtils.cd(target) {
-            FileUtils.mv("index.html", "blog.html")
+           # FileUtils.mv("index.html", "blog.html")
             FileUtils.mv("rss.xml", "blogrss.xml")
+            FileUtils.cp("../cookies.php", "cookies.php")
+            FileUtils.cp("../login.php", "login.php")
         }
       end
     end #Pages
